196bd191340075d657f87cfabbdfc62e5c75331e
anastasia <cyanidecalcium@yandex.ru>
2019-12-27 07:38:28 +0300

    make it better

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ApplicationRecord
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
   belongs_to :category
-
-  validates :title, :preview, :body, presence: true #проверка на непустое значение
+  validates :body, :preview
+  validates :title, presence: true #проверка на непустое значение
 
   def all_tags
     self.tags.map(&:name).join(', ')

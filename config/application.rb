7214ff08d63024e71b59c83335ad8b177541422d
anastasia <cyanidecalcium@yandex.ru>
2019-12-27 06:16:14 +0300

    make it better

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+require "active_support/logger_silence"
+
 
 module Untitled
   class Application < Rails::Application

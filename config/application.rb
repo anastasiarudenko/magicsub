58f0409886eed0bcb74b0e904c71823dbf77f358
anastasia <cyanidecalcium@yandex.ru>
2019-12-27 05:45:41 +0300

    make it better

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Untitled
   class Application < Rails::Application
+    include ActiveSupport::LoggerSilence
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 

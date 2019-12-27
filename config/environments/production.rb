3f91181fcf1ee7e120eba14445773caa03ae3b6b
anastasia <cyanidecalcium@yandex.ru>
2019-12-27 06:35:36 +0300

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address   => "smtp.gmail.com",
+      :port      => 587,
+      :enable_starttls_auto => true,
+      :user_name => "magsubtitle",
+      :password  => "mixQot-bavpiq-mojhy5",
+      :authentication => 'login',
+      :domain => 'gmail.com',
+  }
 end

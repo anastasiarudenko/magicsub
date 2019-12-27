58e54a3f3e7d0e8ed4cf4121f817339f1c42baa8
anastasia <cyanidecalcium@yandex.ru>
2019-12-27 06:12:34 +0300

    make it better

--- config/application.rb
+++ config/application.rb
@@ -4,20 +4,12 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(ActiveSupport::LoggerSilence.groups)
 
 module Untitled
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    config.log_level = ENV.fetch('LOG_LEVEL', 'INFO').downcase
-
-    if ENV['RAILS_LOG_TO_STDOUT'].present?
-      logger           = ActiveSupport::Logger.new(STDOUT)
-      logger.level     = ActiveSupport::Logger.const_get(config.log_level.to_s.upcase)
-      config.logger    = ActiveSupport::TaggedLogging.new(logger)
-    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

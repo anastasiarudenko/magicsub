c1bd7f74b27d6ea9f80d30318d010dad186bb8a3
anastasia <cyanidecalcium@yandex.ru>
2019-12-27 06:01:31 +0300

    make it better

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module Untitled
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.log_level = ENV.fetch('LOG_LEVEL', 'INFO').downcase
+
+    if ENV['RAILS_LOG_TO_STDOUT'].present?
+      logger           = ActiveSupport::Logger.new(STDOUT)
+      logger.level     = ActiveSupport::Logger.const_get(config.log_level.to_s.upcase)
+      config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

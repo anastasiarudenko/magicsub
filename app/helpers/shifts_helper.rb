0faad883bd8cbfb58eb05e8f5b7eb2540dfb3b28
anastasia <cyanidecalcium@yandex.ru>
2020-01-06 17:10:23 +0300

    fixed encoding (probably)

--- app/helpers/shifts_helper.rb
+++ app/helpers/shifts_helper.rb
@@ -6,18 +6,29 @@ module ShiftsHelper
     require 'time'
 
     b = @shift.time
+    s = false
+    enc = 'a'
 
     lines.each_index do |n|
+      if ! lines[n].valid_encoding?
+        lines[n] = lines[n].encode("UTF-8", "Windows-1252")
+        s = true
+        enc = 'w:windows-1252'
+      end
       if /\d\d:\d\d:\d\d,\d\d\d\s-->\s\d\d:\d\d:\d\d,\d\d\d/ === lines[n].chomp
         time = Time.parse(lines[n][0..7]) + b
         lines[n][0..7] = time.strftime('%H:%M:%S')
         time = Time.parse(lines[n][17..24]) + b
         lines[n][17..24] = time.strftime('%H:%M:%S')
       end
+      if s == true
+        lines[n] = lines[n].encode("Windows-1252", "UTF-8")
+        s = false
+      end
     end
 
     File.open(a, 'a') { |file| file.truncate(0) }
-    File.open(a, 'a') { |file| lines.each { |x| file.puts(x) } }
+    File.open(a, enc) { |file| lines.each { |x| file.puts(x) } }
   end
 end
 
